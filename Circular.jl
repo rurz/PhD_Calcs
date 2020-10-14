a9cac7de40ac6d5b9f074e6a62726e81d5af51fc
Alejandro R. Urzúa <arurz@inaoep.mx>
2020-10-14 14:09:31 -0500

    Added Circular.jl
    First commit on the Pluto.jl port

---
+++ Circular.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.12.3
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 6492430e-0e50-11eb-3419-ff3b319119b7
+md"# Amplitude evolution on a circular array of harmonic elements coupled by nearest neighbour interaction"
+
+# ╔═╡ Cell order:
+# ╠═6492430e-0e50-11eb-3419-ff3b319119b7
